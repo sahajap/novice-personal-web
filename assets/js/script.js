d42ec72913786e74eb1344b3fa6bff78be5f16c7
Sahaja Pinnu <sahajap@umich.edu>
2020-08-15 23:14:44 -0400

    moving words

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,19 @@
+var i = 0;
+var txt = 'Student | Leader | Creator'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typing").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+//when the window loads, run this function
 window.onload = function(){ 
     typeWriter(); 
   }
-  
-  
-  var i = 0;
-  var txt = 'Hey! My name is Oliver, nice to meet you'; /* The text */
-  var speed = 100; /* The speed/duration of the effect in milliseconds */
-  var skip = 0; 
-  
-  
-  function typeWriter() {  
-    if (i < txt.length) {
-      if(txt.charAt(i) == ' '){ 
-        document.getElementById("typing").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, skip);
-      }
-      else{ 
-        document.getElementById("typing").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      }
-  
-    }
-  }
+
+//typing.js (you can import)
+//https://github.com/mattboldt/typed.js/
